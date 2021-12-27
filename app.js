87567ab55de1a304163d2c5b21f771c77eda0a00
Rabia-Rabia <76133508+Rabia-Rabia@users.noreply.github.com>
2021-12-27 23:29:13 +0400

    shopping cart

---
+++ app.js
@@ -0,0 +1,30 @@
+const changeQuantity = (self, action) => {
+    const productQuantityElement = self.closest('.product').querySelector('input[type = "number"]');
+    const productQuantity = parseInt(productQuantityElement.value);
+
+
+    switch (action) {
+        case 'increment':
+            productQuantityElement.value = productQuantity + 1;
+            break;
+        case 'decrement':
+            productQuantityElement.value = productQuantity > 1 ? productQuantity - 1 : 1;
+            break;
+
+    }
+}
+
+document.querySelectorAll('.product-plus').forEach(el => {
+    el.addEventListener('click', function() {
+        // document.querySelector('input[type = "number"]').value = 5;
+        changeQuantity(this, 'increment');
+
+    });
+})
+
+document.querySelectorAll('.product-minus').forEach(el => {
+    el.addEventListener('click', function() {
+        changeQuantity(this, 'decrement');
+
+    });
+})
